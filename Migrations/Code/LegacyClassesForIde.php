22d44c1e0761361c3921c271fc256f97214d15b4
Helmut Hummel <helmut.hummel@typo3.org>
2015-07-07 18:31:23 +0200

    [TASK] Move t3lib_extMgm alias to compatibility6 extension
    ext_autoload.php is ignored now, so the issues why we kept
    this alias in the core is obsolete.
    
    Releases: master
    Resolves: #67966
    Change-Id: Ibdcf362b5c31e938941b374aff786d81da4668ba
    Reviewed-on: http://review.typo3.org/40969
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Wouter Wolters <typo3@wouterwolters.nl>
    Tested-by: Wouter Wolters <typo3@wouterwolters.nl>

---
+++ Migrations/Code/LegacyClassesForIde.php
@@ -0,0 +1,9 @@
+<?php
+namespace TYPO3\CMS\Rtehtmlarea\Extension;
+
+die('Access denied');
+
+/**
+ * @deprecated since TYPO3 CMS 7, will be removed in TYPO3 CMS 8
+ */
+class Acronym extends \TYPO3\CMS\Rtehtmlarea\Extension\Abbreviation {}
