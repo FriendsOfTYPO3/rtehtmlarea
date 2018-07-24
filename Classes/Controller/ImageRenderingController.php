00818a87aa902a5dca486744d20ab9b2ecb47c3c
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2018-07-24 13:39:18 +0200

    [Bugfix] take care of absRefPrefix for images
    fixes #19

--- Classes/Controller/ImageRenderingController.php
+++ Classes/Controller/ImageRenderingController.php
@@ -16,6 +16,7 @@ namespace TYPO3\CMS\Rtehtmlarea\Controller;
 
 use TYPO3\CMS\Core\Resource;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Service\ImageService;
 
 /**
  * Render the image attributes and reconstruct magic images, if necessary (and possible)
@@ -76,6 +77,7 @@ class ImageRenderingController extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin
             if ($fileUid) {
                 try {
                     $file = Resource\ResourceFactory::getInstance()->getFileObject($fileUid);
+                    $imageService = new ImageService();
                     if ($imageAttributes['src'] !== $file->getPublicUrl()) {
                         // Source file is a processed image
                         $imageConfiguration = [
@@ -84,13 +86,15 @@ class ImageRenderingController extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin
                         ];
                         $processedFile = $this->getMagicImageService()->createMagicImage($file, $imageConfiguration);
                         $additionalAttributes = [
-                            'src' => $processedFile->getPublicUrl(),
+                            'src' => $imageService->getImageUri($processedFile),
                             'title' => $imageAttributes['title'] ?: $file->getProperty('title'),
                             'alt' => $imageAttributes['alt'] ?: $file->getProperty('alternative'),
                             'width' => $processedFile->getProperty('width'),
                             'height' => $processedFile->getProperty('height'),
                         ];
                         $imageAttributes = array_merge($imageAttributes, $additionalAttributes);
+                    } else {
+                        $imageAttributes['src'] = $imageService->getImageUri($file);
                     }
                 } catch (Resource\Exception\FileDoesNotExistException $fileDoesNotExistException) {
                     // Log the fact the file could not be retrieved.
@@ -98,7 +102,9 @@ class ImageRenderingController extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin
                     $this->getLogger()->error($message);
                 }
             }
+
         }
+
         return '<img ' . GeneralUtility::implodeAttributes($imageAttributes, true, true) . ' />';
     }
 
